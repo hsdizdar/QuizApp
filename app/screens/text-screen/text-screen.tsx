93db7acbac9b9660114492ae91f496808526efed
Hasret Suna Dizdar <hasretsuna.dizdar@fibabanka.com.tr>
2020-05-09 17:38:09 +0300

    change

--- app/screens/text-screen/text-screen.tsx
+++ app/screens/text-screen/text-screen.tsx
@@ -1,10 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, useState } from "react"
 import LinearGradient from "react-native-linear-gradient"
+import { TouchableOpacity, Text } from "react-native"
 
-export const TextScreen: FunctionComponent = () => (
-  <LinearGradient
-    colors={["#005BEA", "#00C6FB"]}
-    style={{ flex: 1, borderRadius: 20 }}
-  ></LinearGradient>
-)
+export const TextScreen: FunctionComponent = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <LinearGradient
+      colors={["#005BEA", "#00C6FB"]}
+      style={{ flex: 1, borderRadius: 20, padding: 20 }}
+    >
+      <Text>{count}</Text>
+      <TouchableOpacity onPress={() => setCount(count + 1)}>
+        <Text>+</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  )
+}
